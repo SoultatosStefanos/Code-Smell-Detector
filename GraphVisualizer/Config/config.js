b4f9b59c88ce94a748d4ba0c31d2ce5d4f0cd0c8
Krystallia Savvaki <41945729+krystalsavv@users.noreply.github.com>
2020-09-22 12:09:01 +0300

    Condition check in UI using eval()

--- GraphVisualizer/Config/config.js
+++ GraphVisualizer/Config/config.js
@@ -1,6 +1,6 @@
 (async () => {
     const json = await fetch("/Config/config.json").then(response => response.json());
-
+    
     var config = new Vue({
         el: '#config',
         data: {
@@ -8,14 +8,8 @@
             groups: json.groups
         },
         methods: {
-            conditionCheck(obj) {
-                if (eval(obj.condition)) {
-                    console.log("condition: true");
-                    // den to briskei giati den exei oristei akoma 
-                    // eval("document.getElementById(\""+ obj.bindTo + "\")."+ obj.result + " = " +  obj.resultValue); 
-                } else {
-                    console.log("condition: false");
-                }
+            evalCondition(condition) {
+                return eval(condition);
             }
         }
     });
