86a7553bb0132164d9d02cbec76dcec3929110a7
Krystallia Savvaki <krystalliasav@gmail.com>
2021-02-05 19:28:40 +0200

    change main argument flag names ("--src", "--cmp-db")

--- GraphGenerator/Main/Architecture Mining.cpp
+++ GraphGenerator/Main/Architecture Mining.cpp
@@ -9,8 +9,8 @@
 
 static void PrintMainArgInfo(void) {
 	std::cout << "MAIN ARGUMENTS:\n\n";
-	std::cout << "argv[1]: \"--dir\" to mine whole directory (argv[2]: directory)\n";
-	std::cout << "argv[1]: \"--cmp_db\" to use compilation database (argv[2]: path/to/compile_commands.json)\n";
+	std::cout << "argv[1]: \"--src\" to mine whole directory with sources (argv[2]: directory/with/sources)\n";
+	std::cout << "argv[1]: \"--cmp-db\" to use compilation database (argv[2]: path/to/compile_commands.json)\n";
 	std::cout << "argv[3]: (file path) path/to/ignoredFilePaths\n";
 	std::cout << "argv[4]: (file path) path/to/ignoredNamespaces\n";
 	std::cout << "argv[5]: (file path) path/to/output\n";
@@ -21,23 +21,23 @@ int main(int argc, const char** argv) {
 		PrintMainArgInfo();
 		return 1;
 	}
-	std::string option1 = "--dir";
-	std::string option2 = "--cmp_db";
+	std::string option1 = "--src";
+	std::string option2 = "--cmp-db";
 	const char* cmpDBPath = nullptr;
 	std::vector<std::string> srcs;
-	if (option1 == argv[1]) { // --dir
+	if (option1 == argv[1]) { // --src
 		sourceLoader::SourceLoader srcLoader(argv[2]);
 		srcLoader.LoadSources();
 		srcs = srcLoader.GetSources();
 	}
-	else if (option2 == argv[1]) { //--cmp_db
+	else if (option2 == argv[1]) { //--cmp-db
 		cmpDBPath = argv[2];
 	}
 	else {
 		PrintMainArgInfo();
 		return 1;
 	}
-	const char* option = argv[1]; // option = "--dir", option = "--cmp_db"
+	const char* option = argv[1]; // option = "--src", option = "--cmp-db"
 	
 	const char* ignoredFilePaths = (argc >= 4) ? argv[3] : "";
 	const char* ignoredNamespaces = (argc >= 5) ? argv[4] : "";
