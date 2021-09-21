de95e675b28bd4895c347e14d9c70ef3d30e6a2d
Phivos Papapanagiotakis <phoivos@windowslive.com>
2021-09-21 21:49:30 +0200

    DevTools no longer open by default in the GUI

--- CodeSmellDetector/code-smell-detector-GUI/src/index.js
+++ CodeSmellDetector/code-smell-detector-GUI/src/index.js
@@ -22,7 +22,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
 //   main_script.initialize();
 };
