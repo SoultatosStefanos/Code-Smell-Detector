f1ecf6320513ce1d8fda3593151ea03b941d8aa2
SoultatosStefanos <stefanoss1498@gmail.com>
2022-06-03 11:35:43 +0300

    Testing the import functions

--- GraphGenerator/UnitTests/IncrementalTests.cpp
+++ GraphGenerator/UnitTests/IncrementalTests.cpp
@@ -11,6 +11,7 @@ namespace {
 	using namespace incremental;
 	using namespace dependenciesMining;
 
+	// Useful for out-of-source builds.
 	inline auto ResolvePath(const std::string& to, std::string from = __FILE__) {
 		const auto file_directory = std::string(dirname(from.data()));
 		auto resolved_path = file_directory + "/" + to;
@@ -24,12 +25,45 @@ namespace {
 		return std::count_if(std::begin(table), std::end(table), countAll) == 0;
 	}
 
+	inline void ExportST(SymbolTable& table,const std::string_view jsonPath) {
+		assert(!std::filesystem::exists(jsonPath));
+
+		Json::Value jsonST;
+		table.AddJsonSymbolTable(jsonST["structures"]);
+		std::ofstream jsonSTFile{jsonPath.data()};
+		jsonSTFile << jsonST;
+
+		assert(std::filesystem::exists(jsonPath));
+	}
+
+	// RAI With temp file
+	struct TempFile {
+		TempFile(const std::string_view fname) : file{fname} {}
+		~TempFile() { std::remove(file.data()); }
+
+		operator std::string_view() const {return file;} 
+
+		const std::string_view file;
+	};
+
+	[[nodiscard]] inline auto MakeTempFile(const std::string_view fname) { return TempFile{fname}; }
+
 	TEST(ImportStashedST, Imports_nothing_from_non_existent_path) {
-		const auto path = ResolvePath("NonExistent.json");
+		const auto path = ResolvePath("Out.json");
 		assert(!std::filesystem::exists(path));
 		SymbolTable table;
 
-		ImportStashedST(path.data(), table);
+		ImportStashedST(path, table);
+
+		ASSERT_TRUE(IsEmpty(table));
+	}
+
+	TEST(ImportStashedST, Imports_nothing_from_empty_json) {
+		const auto tmp = MakeTempFile(ResolvePath("Out.json"));
+		SymbolTable table;
+		ExportST(table, tmp);
+
+		ImportStashedST(tmp, table);
 
 		ASSERT_TRUE(IsEmpty(table));
 	}
